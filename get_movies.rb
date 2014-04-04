b460701f8c281e013314d3b0f2e99d85c68d57e3
Joanne Cheng <joanne@thoughtbot.com>
2014-04-03 18:31:20 -0600

    initial commit

---
+++ get_movies.rb
@@ -0,0 +1,25 @@
+require 'pry'
+require 'csv'
+
+year_re = /(\d{4})/
+movie_name_and_result_re = /\[(\d)\]\](.*)\[/
+
+lines = open('movie_list.txt').read.split("\n")
+current_year = 2014
+movies = []
+
+#binding.pry
+lines.each do |line|
+  if !year_re.match(line).nil?
+    current_year = year_re.match(line)[1]
+  elsif !movie_name_and_result_re.match(line).nil?
+    matches = movie_name_and_result_re.match(line)
+    movies << [current_year, matches[1], matches[2].strip]
+  end
+end
+
+CSV.open('movies.csv', 'w') do |csv|
+  movies.each do |movie|
+    csv << movie
+  end
+end
